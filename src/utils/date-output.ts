5f337d24a6a9e5b639238119dbb72ce712db5bf6
hassanzohdy <hassanzohdy@gmail.com>
2023-09-06 02:26:33 +0300

    "Fixed date output"

--- src/utils/date-output.ts
+++ src/utils/date-output.ts
@@ -4,18 +4,24 @@ export function dateOutput(date: Date | any, format = "DD-MM-YYYY hh:mm:ss A") {
   // if format and timestamp exists, it means that the value is already parsed
   if (!date || (date?.format && date?.timestamp)) return date;
 
-  const dayjsDate = dayjs(date);
+  try {
+    const dayjsDate = dayjs(date);
 
-  return {
-    format: dayjsDate.format(format),
-    timestamp: dayjsDate.valueOf(),
-    humanTime: (dayjsDate as any).fromNow(),
-    text: new Intl.DateTimeFormat("en-US", {
-      dateStyle: "long",
-      timeStyle: "medium",
-    }).format(dayjsDate.toDate()),
-    date: new Intl.DateTimeFormat("en-US", {
-      dateStyle: "long",
-    }).format(dayjsDate.toDate()),
-  };
+    const dateObject = dayjsDate.toDate();
+
+    return {
+      format: dayjsDate.format(format),
+      timestamp: dayjsDate.valueOf(),
+      humanTime: (dayjsDate as any).fromNow(),
+      text: new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+        timeStyle: "medium",
+      }).format(dateObject),
+      date: new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+      }).format(dateObject),
+    };
+  } catch (error: any) {
+    return;
+  }
 }
