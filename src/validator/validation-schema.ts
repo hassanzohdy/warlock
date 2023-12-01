0a703f7320ea326401898b0bc8c68149400382f7
hassanzohdy <hassanzohdy@gmail.com>
2023-12-01 18:23:36 +0200

    "Added validation schema"

--- src/validator/validation-schema.ts
+++ src/validator/validation-schema.ts
@@ -1,12 +1,14 @@
 import type { Rule } from "./rules/rule";
 
+export type RuleType = string | Rule;
+
 export class ValidationSchema {
   /**
    * constructor
    */
   public constructor(
     public readonly inputs: {
-      [key: string]: (string | Rule)[] | ValidationSchema;
+      [key: string]: RuleType[] | ValidationSchema;
     },
     public readonly isArraySchema = false,
   ) {
@@ -26,6 +28,24 @@ export class ValidationSchema {
         }
       }
     }
+
+    return this;
+  }
+
+  /**
+   * Apply the given rules to all inputs
+   */
+  public rules(rules: RuleType[]) {
+    for (const input in this.inputs) {
+      const list = this.inputs[input];
+      if (Array.isArray(list)) {
+        this.inputs[input] = rules.concat(list);
+      } else {
+        list.rules(rules);
+      }
+    }
+
+    return this;
   }
 }
 
